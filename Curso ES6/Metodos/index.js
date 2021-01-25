39d4edcec99f22369d0d45d10cb1858718af11b1
Sebastian Flores <sebastian.floresred.97@gmail.com>
2021-01-25 18:56:11 -0300

    commit 1

---
+++ Curso ES6/Metodos/index.js
@@ -0,0 +1,33 @@
+// METODOS 
+
+let cadena = 'Hola Raccoon';
+
+// length => devuelve el largo
+
+console.log(cadena.length);
+
+// toUpperCase() => devuelve todo en mayusculas
+
+console.log(cadena.toUpperCase());
+
+// toLowerCase() => devuelve todo en miniscula
+
+console.log(cadena.toLowerCase());
+
+// indexOf() => devuelve la posicion en la que se encuentre el string
+
+console.log(cadena.indexOf('R'));
+
+// replace() => reemplaza el fragmento de la cadena por un valor nuevo
+
+console.log(cadena.replace('Raccoon', 'Mapache'));
+
+// substring() => extrae los caracteres desde donde se indica
+
+console.log(cadena.substring(6));
+console.log(cadena.substring(6, 9));
+
+// slice() =>
+
+console.log(cadena.slice(6, 9));
+console.log(cadena.slice(-6));
